951223616a92100d6542ab46d261c22253daa13c
elhagin <omar.elhagin@live.com>
2016-12-02 18:36:11 +0100

    Initial commit

---
+++ Assets/AndysComing.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndysComing : MonoBehaviour {
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			anim.SetBool ("Andy", true);
+		}
+
+		if (Input.GetKeyDown(KeyCode.E)) {
+			anim.SetBool ("Andy", false);
+		}
+	}
+}
